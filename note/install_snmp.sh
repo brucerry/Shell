dbdb95decc51e7ed59284df3be5f99ff326f4099
bruce <bruce.cheung111@gmail.com>
2024-02-20 11:40:02 +0800

    Create install_snmp.sh

---
+++ note/install_snmp.sh
@@ -0,0 +1,8 @@
+# Install SNMP in linux
+sudo apt install -y snmpd snmp libsnmp-dev snmp-mibs-downloader
+
+# Edit the snmpd.conf file
+sudo vim /etc/snmp/snmpd.conf
+
+# Reload the snmpd service
+sudo service snmpd restart
