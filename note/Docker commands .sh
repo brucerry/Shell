9ca386e9d8e9ccc96064a34ca43776d87541ba4a
bruce <bruce.cheung111@gmail.com>
2024-03-24 15:37:34 +0800

    Update Docker commands .sh

--- note/Docker commands .sh
+++ note/Docker commands .sh
@@ -30,3 +30,16 @@ docker run -it -v local/path/:/home/example/path new_image_name
 # Execute container bash
 #
 docker exec -it container_name bash
+
+###############################################
+###############################################
+docker login
+docker commit ubt16.04 ubt16.04
+docker tag ubt16.04 brucerry/ubt16.04
+docker push brucerry/ubt16.04
+docker commit ubt18.04 ubt18.04
+docker tag ubt18.04 brucerry/ubt18.04
+docker push brucerry/ubt18.04
+docker commit ubt22.04 ubt22.04
+docker tag ubt22.04 brucerry/ubt22.04
+docker push brucerry/ubt22.04
