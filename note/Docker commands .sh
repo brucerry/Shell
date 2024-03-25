080c1b99e6438407503a811efa5f2bdd531e8a36
bruce <bruce.cheung111@gmail.com>
2024-03-25 11:27:28 +0800

    Update Docker commands .sh

--- note/Docker commands .sh
+++ note/Docker commands .sh
@@ -24,7 +24,7 @@ docker push brucerry/image_name
 # Commit existing container along with changes, and Run new container with new mount
 #
 docker commit container_name new_image_name
-docker run -it -v local/path/:/home/example/path new_image_name
+docker run -it --name new_container_name -v local/path/:/home/example/path new_image_name
 
 #
 # Execute container bash
