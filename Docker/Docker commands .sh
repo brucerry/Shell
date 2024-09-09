7041b132dab352e2ef55141573d98d24bd45de47
bruce <bruce.cheung111@gmail.com>
2024-09-09 16:54:20 +0800

    Update Docker commands .sh

--- Docker/Docker commands .sh
+++ Docker/Docker commands .sh
@@ -11,25 +11,25 @@ docker ps -a
 #
 # Pull image
 #
-docker pull brucerry/image_name
+docker pull brucerry/{image_name}
 
 #
 # Push image
 #
-docker tag image_name brucerry/image_name
+docker tag image_name brucerry/{image_name}
 docker login
-docker push brucerry/image_name
+docker push brucerry/{image_name}
 
 #
 # Commit existing container along with changes, and Run new container with new mount
 #
-docker commit container_name new_image_name
-docker run -it --name new_container_name -v local/path/:/home/example/path new_image_name
+docker commit {container_name} {new_image_name}
+docker run -it --name {new_container_name} -v {local/path/}:{/home/example/path} {new_image_name}
 
 #
 # Execute container bash
 #
-docker exec -it container_name bash
+docker exec -it {container_name} bash
 
 ###############################################
 ###############################################
